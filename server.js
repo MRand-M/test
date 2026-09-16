00d2b7059f1e2a4d622e11b084c8f61686097c6b
MRand-M <mah31324@gmail.com>
2026-09-16 13:41:35 +1000

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const TARGET = "https://1939.giaory.xyz";
 ==========================================================
 */
 
-const TARGET = "https://yanderesimulator.com/";
+const TARGET = "https://wiki.warthunder.com";
 
 
 /*
